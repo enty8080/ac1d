60e4f3875042774afe45c404d0b25d5b1d4bd914
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 18:32:22 +0300

    Update ac1d.m

--- ac1d.m
+++ ac1d.m
@@ -2,8 +2,17 @@
 
 @implementation ac1d
 
--(void)showAlert:(NSString *)title :(NSString *)message {
-    NULL;
+-(void)showAlert:(NSString *)title :(NSString *)message :(NSString *)first_button :(NSString *)second_button {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:first_button];
+    [alert addButtonWithTitle:second_button];
+    [alert setMessageText:title];
+    [alert setInformativeText:message];
+    [alert setAlertStyle:NSWarningAlertStyle];
+
+    if ([alert runModal] == NSAlertFirstButtonReturn) {
+        NULL;
+    }
 }
 
 @end
