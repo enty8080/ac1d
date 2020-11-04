fb9fe3d5851f0b95a9831c5a6587e127ad0ce2ac
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 18:11:17 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,2 +1,12 @@
 #import <Foundation/Foundation.h>
-  
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NULL;
+    }
+    if (argc < 3) printf("Usage: ac1d <option> <arguments>\n");
+    else {
+        NULL;
+    }
+    return 0;
+}
