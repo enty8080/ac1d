ba7a9e781e8a6cca68957ad7db82c373d99f4b8a
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 23:19:16 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -4,21 +4,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NULL;
-    }
-    if (argc < 3) printf("Usage: ac1d <option> <arguments>\n");
-    else {
-        NSMutableArray *args = [NSMutableArray array];
-        for (int i = 0; i < argc; i++) {
-            NSString *str = [[NSString alloc] initWithCString:argv[i] encoding:NSUTF8StringEncoding];
-            [args addObject:str];
-        }
-        if ([args[1] isEqualToString:@"alert"]) {
-            if (argc < 6) printf("Usage: ac1d alert <title> <message> <first_button> <second_button>\n");
-            else {
-		ac1d *ac1d_base = [[ac1d alloc] init];
-                [ac1d_base showAlert:args[2]:args[3]:args[4]:args[5]];
+        ac1d_base = [[ac1d alloc] init];
+        if (argc < 2) printf("Usage: ac1d <option>\n");
+        else {
+            NSMutableArray *args = [NSMutableArray array];
+            for (int i = 0; i < argc; i++) {
+                NSString *str = [[NSString alloc] initWithCString:argv[i] encoding:NSUTF8StringEncoding];
+                [args addObject:str];
             }
+            if ([args isEqualToString:@"battery"]) {
+                [ac1d_base battery];
+            } else printf("Usage: ac1d <option>\n");
         }
     }
     return 0;
