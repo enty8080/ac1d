1fb197a31cda52fa2cd2fc6f81d3c1837d2db55a
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 13:46:52 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-void showAlert(NSString args) {
+void showAlert(NSString *args) {
     NSData *jsonData = [args dataUsingEncoding:NSUTF8StringEncoding];
     NSMutableDictionary *uploadargs = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:NULL];
     const char *title = [[NSString stringWithFormat:@"%@",[uploadargs valueForKey:@"title"]] UTF8String];
@@ -19,14 +19,13 @@ void showAlert(NSString args) {
     CFDictionaryAddValue( dict, kCFUserNotificationAlertTopMostKey, kCFBooleanTrue );
     CFNotificationCenterPostNotificationWithOptions( CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("test"),  NULL, NULL, kCFNotificationDeliverImmediately );
     CFUserNotificationCreate( NULL, timeout, flags, &error, dict );
-    [self term];
 }
 
 int main(int argc, const char *argv[])
 {
     @autoreleasepool {
         if (argc < 3) {
-            print("Usage: ac1d <option> <arguments>\n");
+            printf("Usage: ac1d <option> <arguments>\n");
         } else {
             if ([argv[1] isEqualToString:@"alert"]) {
                 showAlert(argv[2]);
