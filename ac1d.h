61da9068d02dca5c254abf7ac67984c486df1b4e
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 18:33:39 +0300

    Update ac1d.h

--- ac1d.h
+++ ac1d.h
@@ -1,4 +1,4 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
--(void)showAlert(NSString *)title :(NSString *)message;
+-(void)showAlert(NSString *)title :(NSString *)message :(NSString *)first_button :(NSString *)second_button;
