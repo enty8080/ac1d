54008201c175fd7ac5bce91c7555531cc2c8fef6
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 23:23:55 +0300

    Update ac1d.m

--- ac1d.m
+++ ac1d.m
@@ -3,30 +3,14 @@
 @implementation ac1d
 
 -(id)init {
-	return self;
+    _thisUIDevice = [UIDevice currentDevice];
+    [_thisUIDevice setBatteryMonitoringEnabled:YES];
+    return self;
 }
 
--(void)showAlert:(NSString *)title :(NSString *)message :(NSString *)first_button :(NSString *)second_button {
-    UIAlertController * alert = [UIAlertController
-				 alertControllerWithTitle:title
-				 message:message
-				 preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction * firstButton = [UIAlertAction
-				 actionWithTitle:first_button
-				 style:UIAlertActionStyleDefault
-				 handler:^(UIAlertAction * action) {
-				     printf("First button tapped.\n");
-				 }];
-    UIAlertAction * secondButton = [UIAlertAction
-				    actionWithTitle:second_button
-				    style:UIAlertActionStyleDefault
-				    handler:^(UIAlertAction * action) {
-					printf("Second button tapped.\n");
-				    }];
-    [alert addAction:firstButton];
-    [alert addAction:secondButton];
-
-//    [self presentViewController:alert animated:YES completion:nil];
+-(void)battery {
+    int battery_level = ([_thisUIDevice batteryLevel] * 100);
+    printf("Battery Level: %d\n", battery_level);
 }
 
 @end
