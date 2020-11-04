2eb1d826c36f8e68633ece431f5a5e29d7a7e842
Ivan Nikolsky <enty8080@gmail.com>
2020-11-04 18:17:10 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import "ac1d.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -6,7 +7,17 @@ int main(int argc, const char * argv[]) {
     }
     if (argc < 3) printf("Usage: ac1d <option> <arguments>\n");
     else {
-        NULL;
+        NSMutableArray *args = [NSMutableArray array];
+        for (int i = 0; i < argc; i++) {
+            NSString *str = [[NSString alloc] initWithCString:argv[i] encoding:NSUTF8StringEncoding];
+            [args addObject:str];
+        }
+        if ([args[1] isEqualToString:@"alert"]) {
+            if (argc < 4) printf("Usage: ac1d alert <title> <message>\n");
+            else {
+                showAlert(args[2], args[3]);
+            }
+        }
     }
     return 0;
 }
