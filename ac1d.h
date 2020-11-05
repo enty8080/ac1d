a627f85f44282d0a1e79beeb5651e58db44ee93a
Ivan Nikolsky <enty8080@gmail.com>
2020-11-05 09:32:47 +0300

    Update ac1d.h

--- ac1d.h
+++ ac1d.h
@@ -1,5 +1,4 @@
 #import <Foundation/Foundation.h>
-#import <AVFoundation/AVFoundation.h>
 #import <CoreFoundation/CoreFoundation.h>
 #import <CoreLocation/CoreLocation.h>
 #import <AudioToolbox/AudioToolbox.h>
