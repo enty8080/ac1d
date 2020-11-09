e4619f09097cb288522c97730deb5e87cfcd72b8
Ivan Nikolsky <enty8080@gmail.com>
2020-11-09 18:53:37 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -21,22 +21,10 @@ int main(int argc, const char *argv[]) {
             }
             if ([args[1] isEqualToString:@"battery"]) {
                 [ac1d_base battery];
-            } else if ([args[1] isEqualToString:@"vibrate"]) {
-                [ac1d_base vibrate];
             } else if ([args[1] isEqualToString:@"locate"]) {
                 [ac1d_base locate];
-            } else if ([args[1] isEqualToString:@"say"]) {
-                if (argc < 3) printf("Usage: ac1d say <message>\n");
-                else {
-                    [ac1d_base say:args[2]];
-                }
             } else if ([args[1] isEqualToString:@"getvol"]) {
                 [ac1d_base getvol];
-            } else if ([args[1] isEqualToString:@"setvol"]) {
-                if (argc < 3) printf("Usage: ac1d setvol <level>\n");
-                else {
-                    [ac1d_base setvol:args[2]];
-                }
             } else if ([args[1] isEqualToString:@"openurl"]) {
                 if (argc < 3) printf("Usage: ac1d openurl <url>\n");
                 else {
