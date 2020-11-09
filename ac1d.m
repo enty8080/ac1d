cc567982b868a5211edbecbb4662edda17ad983a
Ivan Nikolsky <enty8080@gmail.com>
2020-11-09 09:35:29 +0300

    Update ac1d.m

--- ac1d.m
+++ ac1d.m
@@ -86,9 +86,9 @@
     CFArrayRef ary = SBSCopyApplicationDisplayIdentifiers(false, false);
     CFIndex i;
     for (i = 0; i < CFArrayGetCount(ary); i++) {
-        CFStringGetCString(CFArrayGetValueAtIndex(ary, i),buf, sizeof(buf), kCFStringEncodingUTF8);
-        result = [NSString stringWithFormat:@"%@%s\n",result,buf];
-        printf("%s\n", buf);
+        CFStringGetCString(CFArrayGetValueAtIndex(ary, i), buf, sizeof(buf), kCFStringEncodingUTF8);
+        result = [NSString stringWithFormat:@"%@%s\n", result, buf];
+        printf("%s", [result cStringUsingEncoding:NSUTF8StringEncoding]);
     }
 }
 
