6782da49ed43a90a646e731ff1f794e7c104e49e
Ivan Nikolsky <enty8080@gmail.com>
2020-11-10 22:40:10 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -40,9 +40,14 @@ int main(int argc, const char *argv[]) {
             } else if ([args[1] isEqualToString:@"sysinfo"]) {
                 [ac1d_base sysinfo];
             } else if ([args[1] isEqualToString:@"player"]) {
-                if (argc < 3) printf("Usage: ac1d player <option>\n");
+                if (argc < 3) printf("Usage: ac1d player [info]\n");
                 else {
                     [ac1d_base player:args[2]];
+                }
+            } else if ([args[1] isEqualToString:@"torch"]) {
+                if (argc < 3) printf("Usage: ac1d torch [on|off]\n");
+                } else {
+                    [ac1d_base torch:args[2]];
                 }
             } else if ([commands containsObject:args[1]]) {
                 [ac1d_base send_command:args[1]];
