9ab17e01729f0b74625410f887b130c3f8156dcd
Ivan Nikolsky <enty8080@gmail.com>
2020-11-11 20:20:06 +0300

    Update ac1d.m

--- ac1d.m
+++ ac1d.m
@@ -3,35 +3,8 @@
 @implementation ac1d
 
 -(id)init {
-    _thisUIDevice = [UIDevice currentDevice];
-    [_thisUIDevice setBatteryMonitoringEnabled:YES];
     _messagingCenter = [CPDistributedMessagingCenter centerNamed:@"com.ac1d"];
     return self;
-}
-
--(void)battery {
-    int battery_level = ([_thisUIDevice batteryLevel] * 100);
-    printf("%d", battery_level);
-}
-
--(void)locate {
-    CLLocationManager* manager = [[CLLocationManager alloc] init];
-    [manager startUpdatingLocation];
-    CLLocation *location = [manager location];
-    CLLocationCoordinate2D coordinate = [location coordinate];
-    NSString *result = [NSString stringWithFormat:@"Latitude: %f\nLongitude: %f\nhttp://maps.google.com/maps?q=%f,%f", coordinate.latitude, coordinate.longitude, coordinate.latitude, coordinate.longitude];
-    if ((int)(coordinate.latitude + coordinate.longitude) == 0) {
-        result = @"error";
-    }
-    [manager release];
-    printf("%s", [result cStringUsingEncoding:NSUTF8StringEncoding]);
-}
-
--(void)sysinfo {
-    UIDevice *device = [UIDevice currentDevice];
-    int batinfo = ([_thisUIDevice batteryLevel]*100);
-    NSString *info = [NSString stringWithFormat:@"%@ %d %@ %@ %@ %@", [device model], batinfo, [device systemName], [device systemVersion], [device name], [[device identifierForVendor] UUIDString]];
-    printf("%s", [info cStringUsingEncoding:NSUTF8StringEncoding]);
 }
 
 -(void)send_command:(NSString *)command :(NSString *)arg1 :(NSString *)arg2 {
