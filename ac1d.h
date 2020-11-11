bab74c2751610d0c483bc80a38e57c525eeaf456
Ivan Nikolsky <enty8080@gmail.com>
2020-11-11 09:17:05 +0300

    Update ac1d.h

--- ac1d.h
+++ ac1d.h
@@ -18,7 +18,6 @@ extern int SBSLaunchApplicationWithIdentifier(CFStringRef identifier, Boolean su
 @property (retain) UIDevice *thisUIDevice;
 @property (retain) CPDistributedMessagingCenter *messagingCenter;
 
--(void)battery;
 -(void)locate;
 -(void)getvol;
 -(void)openurl:(NSString *)url;
