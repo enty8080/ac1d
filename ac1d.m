1fc7e12dbdfe076d55663d917dee8017c3478c78
Ivan Nikolsky <enty8080@gmail.com>
2020-11-11 18:04:21 +0300

    Update ac1d.m

--- ac1d.m
+++ ac1d.m
@@ -68,7 +68,7 @@
     NSArray *keys = [NSArray arrayWithObjects:@"cmd", @"arg1", @"arg2", nil];
     NSArray *values = [NSArray arrayWithObjects:command, arg1, arg2, nil];
     NSDictionary *userInfo = [NSDictionary dictionaryWithObjects:values forKeys:keys];
-    NSDictionary *reply = [_messagingCenter sendMessageAndReceiveReplyName:@"commandWithReply" userInfo:userInfo];
+    NSDictionary *reply = [_messagingCenter sendMessageAndReceiveReplyName:@"recieve_command" userInfo:userInfo];
     NSString *replystr = [reply objectForKey:@"returnStatus"];
     printf("%s", [replystr cStringUsingEncoding:NSUTF8StringEncoding]);
 }
