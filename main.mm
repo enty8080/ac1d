ef00a320fcc338c6b40cc0f794b8b647f7ac0ad4
Ivan Nikolsky <enty8080@gmail.com>
2020-11-12 19:23:57 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,6 +1,6 @@
 #import "ac1d.h"
 
-NSArray *commands = [[NSArray alloc] initWithObjects: @"alert", @"battery", @"dhome", @"getvol", @"home", @"location", @"player", @"say", @"setvol", @"state", @"openurl", nil];
+NSArray *commands = [[NSArray alloc] initWithObjects: @"alert", @"battery", @"dhome", @"getvol", @"home", @"location", @"player", @"say", @"setvol", @"state", @"openurl", @"openapp", nil];
 
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
@@ -12,12 +12,7 @@ int main(int argc, const char *argv[]) {
                 NSString *str = [[NSString alloc] initWithCString:argv[i] encoding:NSUTF8StringEncoding];
                 [args addObject:str];
             }
-            if ([args[1] isEqualToString:@"openapp"]) {
-                if (argc < 3) printf("Usage: ac1d openapp <application>\n");
-                else {
-                    [ac1d_base openapp:args[2]];
-                }
-            } else if ([commands containsObject:args[1]]) [ac1d_base send_command:args];
+            if ([commands containsObject:args[1]]) [ac1d_base send_command:args];
             else printf("Usage: ac1d <option>\n");
         }
     }
