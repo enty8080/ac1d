af0731350b4bbd4dd3ff195013100b0a8785a81f
Ivan Nikolsky <enty8080@gmail.com>
2020-11-12 13:52:30 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -33,34 +33,7 @@ int main(int argc, const char *argv[]) {
                     [ac1d_base openapp:args[2]];
                 }
             } else if ([commands containsObject:args[1]]) {
-                if ([args[1] isEqualToString:@"alert"]) {
-                    if (argc < 4) printf("Usage: ac1d alert <title> <message>\n");
-                    else {
-                        [ac1d_base send_command:args];
-                    }
-                } else if ([args[1] isEqualToString:@"player"]) {
-                    if (argc < 3) printf("Usage: ac1d player [next|prev|play|pause|info]\n");
-                    else {
-                        [ac1d_base send_command:args];
-                    }
-                } else if ([args[1] isEqualToString:@"location"]) {
-                    if (argc < 3) printf("Usage: ac1d location [on|off|info]\n");
-                    else {
-                        [ac1d_base send_command:args];
-                    }
-                } else if ([args[1] isEqualToString:@"setvol"]) {
-                    if (argc < 3) printf("Usage: ac1d setvol <level>\n");
-                    else {
-                        [ac1d_base send_command:args];
-                    }
-                } else if ([args[1] isEqualToString:@"say"]) {
-                    if (argc < 3) printf("Usage: ac1d say <message>\n");
-                    else {
-                        [ac1d_base send_command:args];
-                    }
-                } else {
-                    [ac1d_base send_command:args];
-                }
+                [ac1d_base send_command:args];
             } else printf("Usage: ac1d <option>\n");
         }
     }
