5522e06a9a98d8aa607bbf4130e024f591ec6b3d
Ivan Nikolsky <enty8080@gmail.com>
2020-11-12 14:29:43 +0300

    Update ac1d.h

--- ac1d.h
+++ ac1d.h
@@ -1,13 +1,11 @@
 #import <Foundation/Foundation.h>
 #import <AppSupport/CPDistributedMessagingCenter.h>
-#import <CoreFoundation/CoreFoundation.h>
 
 #include "SpringBoardServices/SpringBoardServices.h"
 
 @interface ac1d : NSObject {
 }
 
-CFArrayRef SBSCopyApplicationDisplayIdentifiers(bool onlyActive, bool debuggable);
 extern int SBSLaunchApplicationWithIdentifier(CFStringRef identifier, Boolean suspended);
 
 @property (retain) CPDistributedMessagingCenter *messagingCenter;
