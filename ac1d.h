b7209688ad75c3e465a4400bd27acf275da55554
Ivan Nikolsky <enty8080@gmail.com>
2020-11-17 12:35:56 +0300

    Manual control

--- ac1d.h
+++ ac1d.h
@@ -24,6 +24,16 @@
 
 #import <Foundation/Foundation.h>
 #import <AppSupport/CPDistributedMessagingCenter.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <util.h>
+#include <sys/ttycom.h>
+#include <unistd.h>
+#include <dirent.h>
 
 @interface ac1d : NSObject {
 }
